6c908f298304f71489d8a0c3711436e09b95255e
robie09 <you@example.com>
2021-01-25 19:20:34 +0300

    push darck & light mode

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { GlobalStyle, ThemeButton } from "./styles";
 import CookieList from "./components/ProductList";
 import Home from "./components/Home";
 import { ThemeProvider } from "styled-components";
+import { useState } from "react";
 
 const theme = {
   light: {
@@ -22,12 +23,20 @@ const theme = {
 };
 
 function App() {
+  const [currentTheme, setCurrentTheme] = useState("light");
+
+  const toggleTheme = () => {
+    alert(`Mood change to ${currentTheme}`);
+    setCurrentTheme(currentTheme === "light" ? "dark" : "light");
+  };
+
   return (
-    <ThemeProvider theme={theme.light}>
+    <ThemeProvider theme={theme[currentTheme]}>
       <GlobalStyle />
-      <ThemeButton onClick={() => alert("I do nothing..")}>
-        Dark Theme
+      <ThemeButton onClick={toggleTheme}>
+        {currentTheme === "light" ? "Dark" : "Light"} Mode
       </ThemeButton>
+      alert("Mood change");
       <Home />
       <CookieList />
     </ThemeProvider>
